b6b8a62f610a737c425bda68368979f9298dd161
ccnusjy <ccnusjy@8b5094b9-db8a-8f91-6073-f7a6dc3030a3>
2010-12-16 07:55:56 +0000

    writing performance was nearly doubled by using QuickLZ to compress the big value.

--- src/fx/sunjoy/test/dirtytest/TestBigValue.java
+++ src/fx/sunjoy/test/dirtytest/TestBigValue.java
@@ -9,13 +9,13 @@ import fx.sunjoy.algo.impl.DiskTreap;
 public class TestBigValue {
 	public static void main(String[] args) throws Exception {
 		DiskTreap<String, Serializable> treap = new DiskTreap<String,Serializable>(64,new File("c:/test/treapdb"),64<<20);
-		ByteBuffer buf = ByteBuffer.allocate(200000);
-		for(int i=0;i<125000;i++){buf.put((byte)(Math.random() * 256));};
+		ByteBuffer buf = ByteBuffer.allocate(20000);
+		for(int i=0;i<20000;i++){buf.put((byte)(Math.random() * 256));};
 		buf.flip();
 		byte[] data = buf.array();
 		
 		long t1 = System.currentTimeMillis();
-		for(int i=0;i<1000000;i++){
+		for(int i=0;i<100000;i++){
 			Integer key = (int) (Math.random()*Integer.MAX_VALUE);
 			//System.out.println(String.format("%010d", key));
 			treap.put("thing"+String.format("%010d", key),data);
